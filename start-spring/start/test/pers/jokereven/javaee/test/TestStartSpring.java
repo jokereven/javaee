fcd2a042e8919d096fca72f53e8ccfe3102a6909
jokereven <zjing125.cn@gmail.com>
2022-03-25 13:16:41 +0800

    feat(start-spring): 完成spring HelloWorld :test:

--- start-spring/start/test/pers/jokereven/javaee/test/TestStartSpring.java
+++ start-spring/start/test/pers/jokereven/javaee/test/TestStartSpring.java
@@ -1,6 +1,7 @@
 package pers.jokereven.javaee.test;
 
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pers.jokereven.javaee.entity.StartSpring;
 
 public class TestStartSpring {
@@ -10,5 +11,16 @@ public class TestStartSpring {
         StartSpring startSpring = new StartSpring();
         startSpring.setWho("joker even");
         startSpring.hello();
+    }
+
+    @Test
+    public void TestSpringStart(){
+//        通过spring 的上下文 context 对象 读取配置文件
+        ClassPathXmlApplicationContext ctx =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+//        使用ctx直接获取spring容器的对象
+//        getBean获取容器对象
+       StartSpring startSpring = (StartSpring) ctx.getBean("startSpring");
+       startSpring.hello();
     }
 }
