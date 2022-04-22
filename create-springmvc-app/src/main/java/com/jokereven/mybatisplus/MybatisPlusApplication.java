e86e7caf2e5dda66ce486d62e480dc68557025d9
jokereven <zjing125.cn@gmail.com>
2022-04-22 09:20:50 +0800

    feat: 初始化 create-springmvc-app

---
+++ create-springmvc-app/src/main/java/com/jokereven/mybatisplus/MybatisPlusApplication.java
@@ -0,0 +1,24 @@
+package com.jokereven.mybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+@Controller
+@SpringBootApplication
+public class MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "hello, now is: " + new Date().toString();
+    }
+}
---
+++ create-springmvc-app/src/main/resources/db/data-h2.sql
@@ -0,0 +1,13 @@
+DELETE FROM user;
+
+INSERT INTO user (id, name, age, email) VALUES
+(1, 'Jone', 18, 'test01@jokereven.com'),
+(2, 'Jack', 20, 'test02@jokereven.com'),
+(3, 'Tom', 28, 'test03@jokereven.com'),
+(4, 'Sandy', 21, 'test04@jokereven.com'),
+(5, 'Billie', 24, 'test05@jokereven.com'),
+(6, 'Taylor', 18, 'test06@jokereven.com'),
+(7, 'Brant', 20, 'test07@jokereven.com'),
+(8, 'Make', 28, 'test08@jokereven.com'),
+(9, 'Brush', 21, 'test09@jokereven.com'),
+(10, 'Meyer', 24, 'test10@jokereven.com');
